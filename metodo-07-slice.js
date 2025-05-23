7bd6fbd0f31fa79a14aa03b5c1731bb77c37b376
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-05-23 09:59:24 -0300

    Adiciona o arquivo metodo-07-slice.js na pasta Metodo-Array

---
+++ metodo-07-slice.js
@@ -0,0 +1,5 @@
+let playlist = [`Funk`,`Hip-Hop`,`Pop`,`Opera`,`Ninar`,`Louvor`,`Rock`,`Jazz`, ];
+console.log(`Parte da playlist (indices do 2 a 5): ${playlist.slice(2,6)}`);
+console.log(`Parte da playlist (indices do 0 a 3): ${playlist.slice(0,4)}`);
+
+
