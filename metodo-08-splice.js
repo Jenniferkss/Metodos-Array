c636ed8ea942f30e8ba1be3f55050f7550a275bb
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-05-23 10:45:18 -0300

    Modifica o arquivo metodo-08-splice.js

--- metodo-08-splice.js
+++ metodo-08-splice.js
@@ -1,6 +1,10 @@
-let playlist = ['Funk', 'Hip-Hop', 'Pop', 'Rock', 'Jazz'];
+let playlist = ["Funk", "Hip-Hop", "Pop", "Rock", "Jazz", `Hino`, `Louvor`];
 console.log(`Antes do splice: ${playlist}`);
-let elementosRemovidos =playlist.splice(2,2)
+let elementosRemovidos = playlist.splice(2, 2);
 console.log(`Depois do splice: ${playlist}`);
 console.log(`Elementos removidos: ${elementosRemovidos}`);
 
+console.log(`Antes do splice: ${playlist}`);
+let desafio = playlist.splice(3, 2);
+console.log(`Elementos removidos: ${desafio}`);
+console.log(`Depois do splice: ${playlist}`);
