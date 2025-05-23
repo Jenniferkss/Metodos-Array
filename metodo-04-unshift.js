812e5ac6c7a579450d3d943a5f37fd81f3298038
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-05-23 08:57:02 -0300

    Adicona arquivo metodo-04-unshift na pasta Metodos-Array

---
+++ metodo-04-unshift.js
@@ -0,0 +1,7 @@
+let playlist =  [`Louvor`,`Pop`,`Trap`];
+console.log(`Antes do unshift: ${playlist}`);
+
+//Adicionando nova musica no inicio da playlist 
+playlist.unshift(`Trap Gospel`);
+
+console.log(`Depois do unshift: ${playlist}`);
