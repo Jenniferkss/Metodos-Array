3b7ce82aa268ea5cd1fa3dfc62daa9b08efe52b9
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-05-23 10:27:55 -0300

    Adiciona arquivo metodo-08-slice.js na pasta Metodos-Array

---
+++ metodo-08-splice.js
@@ -0,0 +1,6 @@
+let playlist = ['Funk', 'Hip-Hop', 'Pop', 'Rock', 'Jazz'];
+console.log(`Antes do splice: ${playlist}`);
+let elementosRemovidos =playlist.splice(2,2)
+console.log(`Depois do splice: ${playlist}`);
+console.log(`Elementos removidos: ${elementosRemovidos}`);
+
