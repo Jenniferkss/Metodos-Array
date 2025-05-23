67cdaf95311fcc4d57c32c09859c507daee5e1c8
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-05-23 09:30:20 -0300

    Adiciona arquivo metodo-05-includes.js na pasta Metodos-Array

---
+++ metodo-05-includes.js
@@ -0,0 +1,3 @@
+let playlist = [`Funk`,`Hip-Hop`,`Pop`,`Opera`,`Ninar`,`Louvor`];
+console.log(`A playlist inclui a palavra Funk ? ${playlist.includes("Funk")}`);
+console.log(`A playlist inclui a palavra Rock ? ${playlist.includes("Rock")}`);
