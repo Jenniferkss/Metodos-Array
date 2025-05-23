ae0ced572888f4b796746d038cc0a1802412b700
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-05-23 08:52:35 -0300

    Adicona arquivo metodo-03-shift.js na pasta Metodos-Array

---
+++ metodo-03-shift.js
@@ -0,0 +1,8 @@
+let playlist =  [`Louvor`,`Pop`,`Trap`];
+console.log(`Antes do shift: ${playlist}`);
+
+//Removendo a primeira musica da playlist 
+let musicaRemovida = playlist.shift();
+
+console.log(`Depois do shift: ${playlist}`);
+console.log(`Musica removida: ${musicaRemovida}`);
