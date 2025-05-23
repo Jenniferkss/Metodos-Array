a1539969412b8a2b953401f28c4b08fd3f176418
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-05-23 08:35:46 -0300

    Adiciona arquivos metodo-01-push e metodo-02-pop no repositorio Metodos-Array

---
+++ metodo-01-push.js
@@ -0,0 +1,7 @@
+let playlist = ["Pop","Funk","Sertanjo","Louvour"];
+    console.log(`Antes do push: ${playlist}`);
+
+//Adicionando uma nova musica á playlist
+playlist.push("Trap");
+console.log(`Depois do push: ${playlist}`);
+
---
+++ metodo-02-pop.js
@@ -0,0 +1,12 @@
+let playlist =  [`Louvor`,`Pop`,`Trap`];
+console.log(`Antes do pop: ${playlist}`);
+
+//Removendo a ultima musica da playlist 
+let musicaRemovida = playlist.pop();
+console.log(`Depois do pop: ${playlist}`);
+console.log(`Musica removida: ${musicaRemovida}`);
+
+let segundaMusicaRemovida = playlist.pop();
+console.log (`Segunda musica removida: ${segundaMusicaRemovida}`);
+console.log(`Playlist final: ${playlist}`);
+
